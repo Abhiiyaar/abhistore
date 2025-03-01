6ea95a13b7dfab3ea715eaa11ad47c180749dab0
Abhiiyaar <itsabhigandhi@gmail.com>
2025-03-01 13:17:55 +0530

    added sign in page

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,21 @@
-import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
+import { prisma } from "@/db/prisma";
 
 async function seed() {
-  const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
 
   console.log("Database seeded successfully");
 }
 
-seed();
+seed()
